d304620f4645ebf3be3cceb253ba6580d11e6aa2
Arevik Abajyan <arevik.abajyan@gmail.com>
2017-09-21 13:53:30 +0400

    refinance

--- wp-content/themes/bridge/header.php
+++ wp-content/themes/bridge/header.php
@@ -35,7 +35,7 @@
 
     <div class="div_mmenu">
         <div class="menu_close close">
-            MENU <img src="<?php bloginfo('template_url'); ?>/img/close.png" id="mob_img" /></a>
+            <span class="menu_hide">MENU</span> <img src="<?php bloginfo('template_url'); ?>/img/close.png" id="mob_img" /></a>
         </div>
         <a href="">About Us</a>
         <a href="/#mission" class="close">Mission Statement</a>
