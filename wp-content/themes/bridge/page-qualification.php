dbb29ec53aa3e6e26d3932fa648af331a4218e29
Armen Musaelyan <armen@bigbek.com>
2017-09-25 11:49:20 +0400

    changing form open order and required

--- wp-content/themes/bridge/page-qualification.php
+++ wp-content/themes/bridge/page-qualification.php
@@ -8,106 +8,170 @@ get_header(); ?>
         <h1>THE LOAN PROCESS</h1>
     </div>
     <div class="bg_home_2 ql_content">
-        <ul class="ql_content_ul">
-            <li class="ql-list">
-                <span class="ql-top-pic">
-                <img src="<?php bloginfo('template_url'); ?>/img/ProcessBig-01.svg" alt="" width="150">
-                </span>
-                <span class="clear-both"></span>
-            </li>
-            
-            <!--ql-->
-            <li class="ql-list">
-                <span class="ql-circle">1</span>
-                <span class="ql-txt-block">
-                    <span class="ql-txt-block-title">Let’s Chat</span>
-                    <span class="ql-txt-block-sec-title">
-                        Let’s get to know each other. We would like to know your wants and needs.
-                    </span>
-                </span>
-                <span class="clear-both"></span>
-            </li>
-            <li class="ql-list">
-                <span class="ql-circle">2</span>
-                        <span class="ql-txt-block">
-                            <span class="ql-txt-block-title">Application and Documents Collection</span>
-                            <span class="ql-txt-block-sec-title">
-                                We will need to collect necessary documents in order to be able to review your financial strength.
-                            </span>
-                        </span>
-                <span class="clear-both"></span>
-            </li>
-            <li class="ql-list">
-                <span class="ql-circle">3</span>
-                        <span class="ql-txt-block">
-                            <span class="ql-txt-block-title">Evaluation</span>
-                            <span class="ql-txt-block-sec-title">
-                                This is where we conduct a thorough evaluation of your qualification and structure your loan package.
-                            </span>
-                        </span>
-                <span class="clear-both"></span>
-            </li>
-            <li class="ql-list">
-                <span class="ql-circle">4</span>
-                        <span class="ql-txt-block">
-                            <span class="ql-txt-block-title">Submission</span>
-                            <span class="ql-txt-block-sec-title">
-                                Once we have a complete file, we submit it to underwriting for approval.
-                            </span>
-                        </span>
-                <span class="clear-both"></span>
-            </li>
-            <li class="ql-list">
-                <span class="ql-circle">5</span>
-                        <span class="ql-txt-block">
-                            <span class="ql-txt-block-title">Appraisal</span>
-                            <span class="ql-txt-block-sec-title">
-                                Here is the fun part! After submission, we order an appraisal to determine the value of the property.
-                            </span>
-                        </span>
-                <span class="clear-both"></span>
-            </li>
-            <li class="ql-list">
-                <span class="ql-circle">6</span>
-                        <span class="ql-txt-block">
-                            <span class="ql-txt-block-title">Underwriting</span>
-                            <span class="ql-txt-block-sec-title">
-                                By this time, the file has been underwritten and we have our approval with conditions.
-                            </span>
-                        </span>
-                <span class="clear-both"></span>
-            </li>
-            <li class="ql-list">
-                <span class="ql-circle">7</span>
-                        <span class="ql-txt-block">
-                            <span class="ql-txt-block-title">Clear to draw docs</span>
-                            <span class="ql-txt-block-sec-title">
-                                This means the file has been reviewed, the conditions have been met and the loan docs are ready to be released and signed by the borrower.
-                            </span>
-                        </span>
-                <span class="clear-both"></span>
-            </li>
-            <li class="ql-list">
-                <span class="ql-circle">8</span>
-                        <span class="ql-txt-block">
-                            <span class="ql-txt-block-title">Signing Loan Docs</span>
-                            <span class="ql-txt-block-sec-title">
-                                You are almost done! Once the docs are reviewed by escrow. It will be prepared to be signed and notarized.
-                            </span>
-                        </span>
-                <span class="clear-both"></span>
-            </li>
-            <li class="ql-list ql-list-last">
-                <span class="ql-circle">9</span>
-                        <span class="ql-txt-block">
-                            <span class="ql-txt-block-title">Funding and Closing</span>
-                            <span class="ql-txt-block-sec-title">
-                                And here is the last step. Once loan documents are signed, they will be reviewed by the founder and the money will be sent to escrow for closing.
-                            </span>
-                        </span>
-                <span class="clear-both"></span>
-            </li>
-        </ul>
+
+        <div class="ql_box">
+            <div class="ql_box_top">
+                <img src="<?php bloginfo('template_url'); ?>/img/ql_top_icon.png" alt="icon" />
+            </div>
+
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    1
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Let’s Chat</h3>
+                    Let’s get to know each other. We would like to know your wants and needs.
+                </div>
+                <div class="clear-both"></div>
+            </div>
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    2
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Application and Documents Collection</h3>
+                    We will need to collect necessary documents in order to be able to review your financial strength.
+                </div>
+            </div>
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    2
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Application and Documents Collection</h3>
+                    We will need to collect necessary documents in order to be able to review your financial strength.
+                </div>
+            </div>
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    2
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Application and Documents Collection</h3>
+                    We will need to collect necessary documents in order to be able to review your financial strength.
+                </div>
+            </div>
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    2
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Application and Documents Collection</h3>
+                    We will need to collect necessary documents in order to be able to review your financial strength.
+                </div>
+            </div>
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    2
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Application and Documents Collection</h3>
+                    We will need to collect necessary documents in order to be able to review your financial strength.
+                </div>
+            </div>
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    2
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Application and Documents Collection</h3>
+                    We will need to collect necessary documents in order to be able to review your financial strength.
+                </div>
+            </div>
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    2
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Application and Documents Collection</h3>
+                    We will need to collect necessary documents in order to be able to review your financial strength.
+                </div>
+            </div>
+            <div class="ql_box_item">
+                <div class="ql_box_item_number">
+                    2
+                </div>
+                <div class="ql_box_item_text">
+                    <h3>Application and Documents Collection</h3>
+                    We will need to collect necessary documents in order to be able to review your financial strength.
+                </div>
+            </div>
+        </div>
+
+
+
+<!--            <li class="ql-list">-->
+<!--                <span class="ql-circle">3</span>-->
+<!--                        <span class="ql-txt-block">-->
+<!--                            <span class="ql-txt-block-title">Evaluation</span>-->
+<!--                            <span class="ql-txt-block-sec-title">-->
+<!--                                This is where we conduct a thorough evaluation of your qualification and structure your loan package.-->
+<!--                            </span>-->
+<!--                        </span>-->
+<!--                <span class="clear-both"></span>-->
+<!--            </li>-->
+<!--            <li class="ql-list">-->
+<!--                <span class="ql-circle">4</span>-->
+<!--                        <span class="ql-txt-block">-->
+<!--                            <span class="ql-txt-block-title">Submission</span>-->
+<!--                            <span class="ql-txt-block-sec-title">-->
+<!--                                Once we have a complete file, we submit it to underwriting for approval.-->
+<!--                            </span>-->
+<!--                        </span>-->
+<!--                <span class="clear-both"></span>-->
+<!--            </li>-->
+<!--            <li class="ql-list">-->
+<!--                <span class="ql-circle">5</span>-->
+<!--                        <span class="ql-txt-block">-->
+<!--                            <span class="ql-txt-block-title">Appraisal</span>-->
+<!--                            <span class="ql-txt-block-sec-title">-->
+<!--                                Here is the fun part! After submission, we order an appraisal to determine the value of the property.-->
+<!--                            </span>-->
+<!--                        </span>-->
+<!--                <span class="clear-both"></span>-->
+<!--            </li>-->
+<!--            <li class="ql-list">-->
+<!--                <span class="ql-circle">6</span>-->
+<!--                        <span class="ql-txt-block">-->
+<!--                            <span class="ql-txt-block-title">Underwriting</span>-->
+<!--                            <span class="ql-txt-block-sec-title">-->
+<!--                                By this time, the file has been underwritten and we have our approval with conditions.-->
+<!--                            </span>-->
+<!--                        </span>-->
+<!--                <span class="clear-both"></span>-->
+<!--            </li>-->
+<!--            <li class="ql-list">-->
+<!--                <span class="ql-circle">7</span>-->
+<!--                        <span class="ql-txt-block">-->
+<!--                            <span class="ql-txt-block-title">Clear to draw docs</span>-->
+<!--                            <span class="ql-txt-block-sec-title">-->
+<!--                                This means the file has been reviewed, the conditions have been met and the loan docs are ready to be released and signed by the borrower.-->
+<!--                            </span>-->
+<!--                        </span>-->
+<!--                <span class="clear-both"></span>-->
+<!--            </li>-->
+<!--            <li class="ql-list">-->
+<!--                <span class="ql-circle">8</span>-->
+<!--                        <span class="ql-txt-block">-->
+<!--                            <span class="ql-txt-block-title">Signing Loan Docs</span>-->
+<!--                            <span class="ql-txt-block-sec-title">-->
+<!--                                You are almost done! Once the docs are reviewed by escrow. It will be prepared to be signed and notarized.-->
+<!--                            </span>-->
+<!--                        </span>-->
+<!--                <span class="clear-both"></span>-->
+<!--            </li>-->
+<!--            <li class="ql-list ql-list-last">-->
+<!--                <span class="ql-circle">9</span>-->
+<!--                        <span class="ql-txt-block">-->
+<!--                            <span class="ql-txt-block-title">Funding and Closing</span>-->
+<!--                            <span class="ql-txt-block-sec-title">-->
+<!--                                And here is the last step. Once loan documents are signed, they will be reviewed by the founder and the money will be sent to escrow for closing.-->
+<!--                            </span>-->
+<!--                        </span>-->
+<!--                <span class="clear-both"></span>-->
+<!--            </li>-->
+<!--        </ul>-->
+
     </div>
 
 <?php get_footer(); ?>
