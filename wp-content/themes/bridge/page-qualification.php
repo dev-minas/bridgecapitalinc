e3461a3a472a513fbb06ff0e20adf555e7c324ce
Armen Musaelyan <armen@bigbek.com>
2017-09-25 12:23:02 +0400

    ql page restruct

--- wp-content/themes/bridge/page-qualification.php
+++ wp-content/themes/bridge/page-qualification.php
@@ -22,7 +22,6 @@ get_header(); ?>
                     <h3>Let’s Chat</h3>
                     Let’s get to know each other. We would like to know your wants and needs.
                 </div>
-                <div class="clear-both"></div>
             </div>
             <div class="ql_box_item">
                 <div class="ql_box_item_number">
@@ -35,142 +34,69 @@ get_header(); ?>
             </div>
             <div class="ql_box_item">
                 <div class="ql_box_item_number">
-                    2
+                    3
                 </div>
                 <div class="ql_box_item_text">
-                    <h3>Application and Documents Collection</h3>
-                    We will need to collect necessary documents in order to be able to review your financial strength.
+                    <h3>Evaluation</h3>
+                    This is where we conduct a thorough evaluation of your qualification and structure your loan package.
                 </div>
             </div>
             <div class="ql_box_item">
                 <div class="ql_box_item_number">
-                    2
+                    4
                 </div>
                 <div class="ql_box_item_text">
-                    <h3>Application and Documents Collection</h3>
-                    We will need to collect necessary documents in order to be able to review your financial strength.
+                    <h3>Submission</h3>
+                    Once we have a complete file, we submit it to underwriting for approval.
                 </div>
             </div>
             <div class="ql_box_item">
                 <div class="ql_box_item_number">
-                    2
+                    5
                 </div>
                 <div class="ql_box_item_text">
-                    <h3>Application and Documents Collection</h3>
-                    We will need to collect necessary documents in order to be able to review your financial strength.
+                    <h3>Appraisal</h3>
+                    Here is the fun part! After submission, we order an appraisal to determine the value of the property.
                 </div>
             </div>
             <div class="ql_box_item">
                 <div class="ql_box_item_number">
-                    2
+                    6
                 </div>
                 <div class="ql_box_item_text">
-                    <h3>Application and Documents Collection</h3>
-                    We will need to collect necessary documents in order to be able to review your financial strength.
+                    <h3>Underwriting</h3>
+                    By this time, the file has been underwritten and we have our approval with conditions.
                 </div>
             </div>
             <div class="ql_box_item">
                 <div class="ql_box_item_number">
-                    2
+                    7
                 </div>
                 <div class="ql_box_item_text">
-                    <h3>Application and Documents Collection</h3>
-                    We will need to collect necessary documents in order to be able to review your financial strength.
+                    <h3>Clear to draw docs</h3>
+                    This means the file has been reviewed, the conditions have been met and the loan docs are ready to be released and signed by the borrower.
                 </div>
             </div>
             <div class="ql_box_item">
                 <div class="ql_box_item_number">
-                    2
+                    8
                 </div>
                 <div class="ql_box_item_text">
-                    <h3>Application and Documents Collection</h3>
-                    We will need to collect necessary documents in order to be able to review your financial strength.
+                    <h3>Signing Loan Docs</h3>
+                    You are almost done! Once the docs are reviewed by escrow. It will be prepared to be signed and notarized.
                 </div>
             </div>
             <div class="ql_box_item">
                 <div class="ql_box_item_number">
-                    2
+                    9
                 </div>
                 <div class="ql_box_item_text">
-                    <h3>Application and Documents Collection</h3>
-                    We will need to collect necessary documents in order to be able to review your financial strength.
+                    <h3>Funding and Closing</h3>
+                    And here is the last step. Once loan documents are signed, they will be reviewed by the founder and the money will be sent to escrow for closing.
                 </div>
             </div>
         </div>
 
-
-
-<!--            <li class="ql-list">-->
-<!--                <span class="ql-circle">3</span>-->
-<!--                        <span class="ql-txt-block">-->
-<!--                            <span class="ql-txt-block-title">Evaluation</span>-->
-<!--                            <span class="ql-txt-block-sec-title">-->
-<!--                                This is where we conduct a thorough evaluation of your qualification and structure your loan package.-->
-<!--                            </span>-->
-<!--                        </span>-->
-<!--                <span class="clear-both"></span>-->
-<!--            </li>-->
-<!--            <li class="ql-list">-->
-<!--                <span class="ql-circle">4</span>-->
-<!--                        <span class="ql-txt-block">-->
-<!--                            <span class="ql-txt-block-title">Submission</span>-->
-<!--                            <span class="ql-txt-block-sec-title">-->
-<!--                                Once we have a complete file, we submit it to underwriting for approval.-->
-<!--                            </span>-->
-<!--                        </span>-->
-<!--                <span class="clear-both"></span>-->
-<!--            </li>-->
-<!--            <li class="ql-list">-->
-<!--                <span class="ql-circle">5</span>-->
-<!--                        <span class="ql-txt-block">-->
-<!--                            <span class="ql-txt-block-title">Appraisal</span>-->
-<!--                            <span class="ql-txt-block-sec-title">-->
-<!--                                Here is the fun part! After submission, we order an appraisal to determine the value of the property.-->
-<!--                            </span>-->
-<!--                        </span>-->
-<!--                <span class="clear-both"></span>-->
-<!--            </li>-->
-<!--            <li class="ql-list">-->
-<!--                <span class="ql-circle">6</span>-->
-<!--                        <span class="ql-txt-block">-->
-<!--                            <span class="ql-txt-block-title">Underwriting</span>-->
-<!--                            <span class="ql-txt-block-sec-title">-->
-<!--                                By this time, the file has been underwritten and we have our approval with conditions.-->
-<!--                            </span>-->
-<!--                        </span>-->
-<!--                <span class="clear-both"></span>-->
-<!--            </li>-->
-<!--            <li class="ql-list">-->
-<!--                <span class="ql-circle">7</span>-->
-<!--                        <span class="ql-txt-block">-->
-<!--                            <span class="ql-txt-block-title">Clear to draw docs</span>-->
-<!--                            <span class="ql-txt-block-sec-title">-->
-<!--                                This means the file has been reviewed, the conditions have been met and the loan docs are ready to be released and signed by the borrower.-->
-<!--                            </span>-->
-<!--                        </span>-->
-<!--                <span class="clear-both"></span>-->
-<!--            </li>-->
-<!--            <li class="ql-list">-->
-<!--                <span class="ql-circle">8</span>-->
-<!--                        <span class="ql-txt-block">-->
-<!--                            <span class="ql-txt-block-title">Signing Loan Docs</span>-->
-<!--                            <span class="ql-txt-block-sec-title">-->
-<!--                                You are almost done! Once the docs are reviewed by escrow. It will be prepared to be signed and notarized.-->
-<!--                            </span>-->
-<!--                        </span>-->
-<!--                <span class="clear-both"></span>-->
-<!--            </li>-->
-<!--            <li class="ql-list ql-list-last">-->
-<!--                <span class="ql-circle">9</span>-->
-<!--                        <span class="ql-txt-block">-->
-<!--                            <span class="ql-txt-block-title">Funding and Closing</span>-->
-<!--                            <span class="ql-txt-block-sec-title">-->
-<!--                                And here is the last step. Once loan documents are signed, they will be reviewed by the founder and the money will be sent to escrow for closing.-->
-<!--                            </span>-->
-<!--                        </span>-->
-<!--                <span class="clear-both"></span>-->
-<!--            </li>-->
-<!--        </ul>-->
 
     </div>
 
