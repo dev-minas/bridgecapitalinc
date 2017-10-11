a167cdf5f3270df0f1617260c1db792482af8c15
Armen Musaelyan <armen@bigbek.com>
2017-10-11 11:27:22 +0400

    team content

--- wp-content/themes/bridge/page-team.php
+++ wp-content/themes/bridge/page-team.php
@@ -1,76 +1,7 @@
 <?php get_header(); ?>
 
     <div class="bg_home_2 bg_team">
-        <h1>THE TEAM</h1>
-
-        <div class="team_block">
-            <div class="team_img"><img src="<?php bloginfo('template_url'); ?>/img/Vahe.jpg" /></div>
-            <div class="team_name">Vahe<br/><span>Hayrapetyan</span></div>
-            <div class="team_position hidden">Sr. Executive</div>
-            <div class="team_bio">
-                Vahe Hayrapetian has been in the mortgage industry in 2007.  He started his career as a Loan Office at GMAC Mortgage. Vahe’s hard work, dedication and high ethics helped him to be named as one of GMAC's top 10 mortgage originators for multiple years. His success awarded him the position of Associate District Manager at the Pasadena branch. When GMAC exited retail mortgage channel in 2008, Vahe moved to Countrywide as Sales Manager. Countrywide was the largest originator of single-family mortgages in the country before merging with Bank of America. After the financial meltdown of 2008/09, Vahe continued his career at Skyline Home Loans. Being a visionary and an entrepreneur, he has always worked towards creating an independent mortgage firm.  In January 2017, Vahe Hayrapetian and his business partner Spartak Hovakimyan have lunched Bridge Capital Home Loans.
-            </div>
-        </div>
-
-        <div class="team_block">
-            <div class="team_img"><img src="<?php bloginfo('template_url'); ?>/img/Spartak.jpg" /></div>
-            <div class="team_name">Spartak<br/><span>Hovakimyan</span></div>
-            <div class="team_position hidden">Sr. Executive</div>
-            <div class="team_bio">
-                Spartak Hovakimyan is a Mortgage Broker at Bridge Capital Home Loans servicing the greater Los Angeles area. He began his career as a Broker’s Assistant at William J. Haverly and Associates. In 2006, he joined Fidelity Finance and Real Estate Group in Glendale where was recognized as a Top Producer and a distinguished member of the “Young Professionals Group” within the firm. At Fidelity Finance and Real Estate Group, he was originating and processing loans as well as selling properties for his clients.
-                <br /><br />
-                With understanding of the economic market shift and changes, Spartak has since become a Certified Pre-Foreclosure Specialist having closed over 70 Short Sale transactions within the past 4 years. He holds a Bachelor’s Degree in both Finance and Real Estate from California State University of Northridge. Having a Finance degree and experience as a Loan Originator, he is able to provide his clients with unparalleled knowledge, service and expertise.
-                <br /><br />
-                Spartak works with integrity and enthusiasm and loves his profession! He is energetic and always soliciting fresh ideas and new ways of approach to real estate finance and to servicing his clients. He truly is immersed in his profession and has built a large network of real estate agents and professionals as well as developed with great skill, relationships with lenders, asset managers and short sale negotiators. Fluent in English, Armenian and Russian allows him to cover a wide range of demographic areas and clients.
-                <br /><br />
-                His extraordinary level of personal service and proven track record is what separates Spartak Hovakimyan from the others. In 2013 Spartak joined Allied Lending Group in Glendale. Working on financial side of real estate has always been his preference. In 2016, Spartak joined forces with Vahe Hairapetian and established Bridge Capital Home Loans.  He is now fully committed to mortgage industry and is determined to become one of the biggest mortgage brokers in Los Angeles.
-            </div>
-        </div>
-
-        <div style="clear: both;"></div>
-
-        <div class="team_block">
-            <div class="team_img"><img src="<?php bloginfo('template_url'); ?>/img/Jackie.jpg" /></div>
-            <div class="team_name">Jackie<br/><span>Karapetyan</span></div>
-            <div class="team_position hidden">Sr. Processor</div>
-            <div class="team_bio">
-                Jackie Karapetyan began her career in Financial Industry in the beginning of 1999. After receiving her Bachelors in Finance and Business Management at Woodbury University, she was hired by Fidelity Financial Group as a Jr. Processor. In 2011, she was hired by Banc of California as a loan specialist.  By 2013, she was one of the lead specialists in the company.  Her talents were recognized by Cohen Financial, who, after several attempts, was able to bring her on board as a Sr. Processor. In 2016 Jackie’s family had a new addition. By this time, she was looking for a position in a company near her residence.  In January of 2017 she and Bridge Capital Home Loans came to an agreement, and she joined the team as a Sr. Processor.
-            </div>
-        </div>
-
-        <div class="team_block">
-            <div class="team_img"><img src="<?php bloginfo('template_url'); ?>/img/Ani.jpg" /></div>
-            <div class="team_name">Ani<br/><span>Mehrabi</span></div>
-            <div class="team_position hidden">Sr. Transaction<br />Coordinator</div>
-            <div class="team_bio">
-                Ani is a highly trained and motivated bi-lingual transaction coordinator. With over 7 years of experience she began her career at American Saving Financial Services.  Her admirable personality and high work ethics helped her gain tremendous knowledge of the industry. In a short period of time, she becomes one of the lead transaction coordinators, assisting the top 5 loan officers. Ani’s exceptional customer service skills and commitment to perfection have made her stand out and have taken her to a new level in her career.  In 2016, Ani was recruited by Castle & Cooke Mortgage where she was dedicated to one of the top producing Mortgage Originators in the state.  Throughout her career, she has closed thousands of transactions representing millions of dollars in business.  In mid-2017, Ani meet with Bridge Capital Home Loans’ team and capitalized on an opportunity where she can apply hers skills and  grow with the firm to become part of something great.  Besides enjoying her job, she also loves traveling and spending time with friends and family. On top of being a transaction coordinator Ani is a public notary as well.
-            </div>
-        </div>
-
-        <div class="team_block">
-            <div class="team_img"><img src="<?php bloginfo('template_url'); ?>/img/Liyou.jpg" /></div>
-            <div class="team_name">Liyou<br/><span>Aghazarian</span></div>
-            <div class="team_position hidden">Loan Originator</div>
-            <div class="team_bio">
-                Vahe Hayrapetian has been in the mortgage industry in 2007.  He started his career as a Loan Office at GMAC Mortgage. Vahe’s hard work, dedication and high ethics helped him to be named as one of GMAC's top 10 mortgage originators for multiple years. His success awarded him the position of Associate District Manager at the Pasadena branch. When GMAC exited retail mortgage channel in 2008, Vahe moved to Countrywide as Sales Manager. Countrywide was the largest originator of single-family mortgages in the country before merging with Bank of America. After the financial meltdown of 2008/09, Vahe continued his career at Skyline Home Loans. Being a visionary and an entrepreneur, he has always worked towards creating an independent mortgage firm.  In January 2017, Vahe Hayrapetian and his business partner Spartak Hovakimyan have lunched Bridge Capital Home Loans.
-            </div>
-        </div>
-
-        <div class="team_block">
-            <div class="team_img"><img src="<?php bloginfo('template_url'); ?>/img/Frank.jpg" /></div>
-            <div class="team_name">Frank<br/><span>Chander</span></div>
-            <div class="team_position hidden">Loan Originator</div>
-            <div class="team_bio">
-                NMLS # 561090
-                <br /><br />
-                Growing up in L.A I spent my youth with my dad at his construction sites. I have always been fascinated with homes and developments so in 2010 I started practicing real estate. In 2012 I transitioned to the home lending world and have not looked back. With vast experience in both worlds you can feel confident that I know what it takes to get the job done. Whether you are a first time home buyer or an experienced investor, I can help you with all your lending needs.
-                <br /><br />
-                Giving Back
-                <br /><br />
-                In 2015 I started the Homes for Trees Project. The plan was simple, plant more trees!!!! With every transaction I close, I donate a tree to the city of Glendale under my clients names. You will receive a certificate along with the coordinates to the tree that has been planted for you and your family. The idea was for the tree to grow as your family grows. In 2016 I took on the role of one of the board of directors for Glendale Beautiful. I have since given up my seat on the board but still work very close with them. If you are interested in donating a tree or donating your time please reach out to me.
-            </div>
-        </div>
-
+        <?=get_post(2)->post_content?>
     </div>
 
     <div class="bg_home_2 bg_team_bio">
