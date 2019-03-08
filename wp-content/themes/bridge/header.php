d32ed9e8a8fcd1979603261e34f8fdc1a067ccc9
Armen Musaelyan <armen@bigbek.com>
2019-03-08 13:01:22 +0400

    https adaptation

--- wp-content/themes/bridge/header.php
+++ wp-content/themes/bridge/header.php
@@ -12,8 +12,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/resp.css">
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.js" type="text/javascript"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.15/jquery-ui.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" type="text/javascript"></script>
 
     <?php wp_head(); ?>
 </head>
